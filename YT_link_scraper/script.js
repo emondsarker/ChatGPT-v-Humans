da4e28ef88a344051d29b9773f4c70bccc5d92c8
emondsarker <emon331@gmail.com>
2023-01-30 23:56:29 +0600

    updated scraper to skip links that have errors, now moves on to the next one

---
+++ YT_link_scraper/script.js
@@ -0,0 +1,16 @@
+// import { youtube } from 'scrape-youtube';
+youtube = require("scrape-youtube")
+// const { youtube } = require('scrape-youtube');
+
+youtube.search('School of life', {type: 'video'}).then((results) => {
+    // Unless you specify a custom type you will only receive 'video' results
+    // console.log(results.videos);
+    for(i=0;i<results.videos.length;i++){
+        console.log(results.videos[i].title)
+        // console.log(results.videos[i].link)
+    }
+    for(i=0;i<results.videos.length;i++){
+        // console.log(results.videos[i].title)
+        console.log(results.videos[i].link)
+    }
+});

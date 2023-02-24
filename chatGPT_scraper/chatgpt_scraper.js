ccbc92bc678d566907e8b5ee8620d1e63a4892d2
emondsarker <emon331@gmail.com>
2023-02-24 22:21:45 +0600

    removed scraper, added new notebook

--- chatGPT_scraper/chatgpt_scraper.js
+++
@@ -1,7 +0,0 @@
-import { ChatGPTAPI, ChatGPTUnofficialProxyAPI } from 'chatgpt'
-
-  
-// send a message and wait for the response
-let res = await api2.sendMessage("Have we just witnessed ChatGPT completely transform the working world? I've been using it for a while and I feel like I really don't need to hire that many people for my small business now.")
-console.log(res.text)
-
